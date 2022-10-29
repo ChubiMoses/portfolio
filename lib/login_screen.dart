32ce60a216927e78840ad3c173f5f4fbeaaf5933
mubaola23 <mubarakshuaib3@gmail.com>
2022-10-29 21:34:33 +0100

    Registration validation

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,11 +1,10 @@
-
 import 'dart:ui';
 
 import 'package:customer/register_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -15,82 +14,95 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
+      body: Center(
         child: Padding(
-          padding:  const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: ListView(
-              children:  [
-              const SizedBox(height: 100,),
-              const Center(
-                child: Text("Login", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-                )
+            children: [
+              const SizedBox(
+                height: 100,
               ),
-               const SizedBox(height: 20,),
-               const Card(
+              const Center(
+                  child: Text(
+                "Login",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+              )),
+              const SizedBox(
+                height: 20,
+              ),
+              const Card(
                 elevation: 2.0,
                 child: TextField(
                   decoration: InputDecoration(
-                    label: Text("Email"),
-                    prefixIcon: Icon(Icons.email, color: Colors.red,)
-                  ),
+                      label: Text("Email"),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.red,
+                      )),
                 ),
               ),
               const Card(
                 elevation: 2.0,
                 child: TextField(
-                 decoration: InputDecoration(
+                  decoration: InputDecoration(
                     label: Text("Password"),
-                    prefixIcon: Icon(Icons.lock, color: Colors.red,),
-                    suffixIcon: Icon(Icons.visibility, color: Colors.grey,),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.red,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.visibility,
+                      color: Colors.grey,
+                    ),
                   ),
-                  
                 ),
               ),
-              const SizedBox(height: 20,),
-
+              const SizedBox(
+                height: 20,
+              ),
               const Align(
-                alignment: Alignment.centerRight,
-                child:  Text("Forgot password?", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-                )),
-
-              const SizedBox(height: 30,),
-
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.red
-                ),
-                onPressed:(){
-                },
-                 child: const Padding(
-                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                   child: Text("Login"),
-                 )
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Forgot password?",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  )),
+              const SizedBox(
+                height: 30,
               ),
-
-               const SizedBox(height: 30,),
-
-               Row(
+              TextButton(
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.red),
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: Text("Login"),
+                  )),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
+                children: [
                   const Text("Don't have an account?"),
-                  const SizedBox(width: 5,),
-
+                  const SizedBox(
+                    width: 5,
+                  ),
                   GestureDetector(
-                    onTap:(){
-                       Navigator.pushReplacement(context, MaterialPageRoute(builder: 
-                       (context)=>const RegisterScreen()));
-
-                    },
-                    child: const Text("Register", 
-                    style: TextStyle(color: Colors.red, 
-                    fontWeight: FontWeight.bold),)),
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterScreen()));
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(
+                            color: Colors.red, fontWeight: FontWeight.bold),
+                      )),
                 ],
-               )
-
-
-
-
+              )
             ],
           ),
         ),
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,105 +1,166 @@
-
 import 'dart:ui';
 
 import 'package:customer/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'home_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({ Key? key }) : super(key: key);
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  @override
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  bool obscureText = true;
+  onChange() {
+    setState(() {
+      obscureText = !obscureText;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
+      body: Center(
         child: Padding(
-          padding:  const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: ListView(
-              children:  [
-              const SizedBox(height: 100,),
-              const Center(
-                child: Text("Register", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-                )
-              ),
-               const SizedBox(height: 20,),
-               const Card(
-                elevation: 2.0,
-                child: TextField(
-                  decoration: InputDecoration(
-                    label: Text("Email"),
-                    prefixIcon: Icon(Icons.email, color: Colors.red,)
-                  ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 100,
                 ),
-              ),
-              const Card(
-                elevation: 2.0,
-                child: TextField(
-                 decoration: InputDecoration(
-                    label: Text("Password"),
-                    prefixIcon: Icon(Icons.lock, color: Colors.red,),
-                    suffixIcon: Icon(Icons.visibility, color: Colors.grey,),
-                  ),
-                  
-                ),
-              ),
-               const Card(
-                elevation: 2.0,
-                child: TextField(
-                  decoration: InputDecoration(
-                    label: Text("Phone"),
-                    prefixIcon: Icon(
-                      Icons.phone,
-                      color: Colors.red,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.visibility,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 30,),
-
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.red
-                ),
-                onPressed:(){},
-                 child: const Padding(
-                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                   child: Text("Register"),
-                 )
-              ),
-
-               const SizedBox(height: 30,),
-
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
-                  const Text("Already have an account?"),
-                  const SizedBox(width: 5,),
-
-                  InkWell(
-                    onTap:(){
-                       Navigator.push(context, MaterialPageRoute(builder: 
-                       (context)=>const LoginScreen()));
-                    },
-                    child: const Text("Login", style: TextStyle(color: Colors.red, 
-                    fontWeight: FontWeight.bold),
+                const Center(
+                    child: Text(
+                  "Register",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
                 )),
-                ],
-               )
-
-
-
-
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                Card(
+                  elevation: 2.0,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || !value.contains("@")) {
+                        return "Invalid email";
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        label: Text("Email"),
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.red,
+                        )),
+                  ),
+                ),
+                Card(
+                  elevation: 2.0,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.length < 8) {
+                        return "Invalid password";
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: passwordController,
+                    obscureText: obscureText,
+                    decoration: InputDecoration(
+                      label: const Text("Password"),
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Colors.red,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () => onChange(),
+                        icon: Icon(
+                          obscureText ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 2.0,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.length < 11) {
+                        return "Invalid phone number";
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: phoneController,
+                    // inputFormatters: [TextInputFormatter.withFunction((oldValue, newValue) => )],
+                    decoration: const InputDecoration(
+                      label: Text("Phone"),
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.red),
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                      if (_formKey.currentState!.validate()) {
+                        print("Validation Successful");
+                        print(emailController.text);
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const HomeScreen()));
+                      }
+                    },
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      child: Text("Register"),
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Already have an account?"),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginScreen()));
+                        },
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
